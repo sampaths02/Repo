a16fc3e017a2c1512b6e365e02c6596705f8392d
Suraj Sampath <sampaths@enbridge.com>
2020-02-06 16:49:37 -0600

    submodules

---
+++ Enbridge-dotNet-master/Enbridge-dotNet-master/ClientLibrary/Samples/Work/BoardsSample.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi.Types;
+using Microsoft.TeamFoundation.Work.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.TeamFoundation.Core.WebApi;
+
+namespace Microsoft.Azure.DevOps.ClientSamples.Work
+{
+    [ClientSample(WorkWebConstants.RestArea, "boards")]
+    public class BoardsSample : ClientSample
+    {
+        [ClientSampleMethod]
+        public Board GetTeamStoriesBoard()
+        {
+            VssConnection connection = Context.Connection;
+            WorkHttpClient workClient = connection.GetClient<WorkHttpClient>();
+
+            TeamProjectReference project = ClientSampleHelpers.FindAnyProject(this.Context);
+            WebApiTeamRef team = ClientSampleHelpers.FindAnyTeam(this.Context, project.Id);
+
+            TeamContext context = new TeamContext(project.Id, team.Id);
+            context.Team = team.Name;
+            context.Project = project.Name;          
+
+            Board board = workClient.GetBoardAsync(context, "Stories").Result;
+
+            Context.Log("Columns for 'Stories' Board for Project '{0}' and Team '{1}'", context.Project, context.Team);
+            Context.Log(""); 
+            
+            return board;
+        }
+
+        [ClientSampleMethod]
+        public List<BacklogLevelConfiguration> GetBacklogs()
+        {
+            VssConnection connection = Context.Connection;
+            WorkHttpClient workClient = connection.GetClient<WorkHttpClient>();
+
+            TeamProjectReference project = ClientSampleHelpers.FindAnyProject(this.Context);
+            WebApiTeamRef team = ClientSampleHelpers.FindAnyTeam(this.Context, project.Id);
+
+            TeamContext teamContext = new TeamContext(project.Id, team.Id)
+            {
+                Team = team.Name,
+                Project = project.Name
+            };
+
+            List<BacklogLevelConfiguration> backlogs = workClient.GetBacklogsAsync(teamContext).Result;
+
+            foreach(var backlog in backlogs)
+            {
+                Context.Log("Backlog: '{0}' Type: '{1}'", backlog.Name, backlog.Type);                
+            }
+
+            return backlogs;            
+        }
+    }
+}
